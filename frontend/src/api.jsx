2f7a4b72ac8be958b622915c74bd6b7f980c1d83
Arthemis11 <presapplesang@gmail.com>
2025-04-28 23:11:18 +0800

    removed frontend

--- frontend/src/api.jsx
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-// Setup the base URL for your backend API
-const api = axios.create({
-  baseURL: 'http://localhost:8000/login/api/',  // Change this to your Django API URL
-});
-
-export default api;
